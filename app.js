b6b141a2019d5e0025b1ba45a80a98076d0480d3
TJSchowen <156722441+TJSchowen@users.noreply.github.com>
2025-02-24 22:52:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+//Global Variable
 const weatherAPI = "https://api.open-meteo.com/v1/forecast?latitude=36.1628&longitude=-85.5016&current=temperature_2m,relative_humidity_2m,precipitation,rain,showers,snowfall,weather_code&hourly=precipitation_probability&temperature_unit=fahrenheit&timezone=America%2FChicago"
 
+//Fetches data from meteo api
 fetch(weatherAPI)
     .then(objResponse => objResponse.json())
     .then(objData => {
+        //Stores data into variable
         const temp = objData.current.temperature_2m
         const humi = objData.current.relative_humidity_2m
         const weatherCode = objData.current.weather_code
         
+        //Prints out Tempature and Humidity 
         document.querySelector('#temp').innerHTML = `${temp}&#176;`
         document.querySelector('#humi').textContent = `${humi}%`
         
+        //Option Map for the Weather_code that determiens what current weather conditions are
         const conditions = {
             0: "Clear Sky",
             1: "Partly cloudy",
@@ -28,6 +33,7 @@ fetch(weatherAPI)
             75: "Snowfall",
         }
 
+        //Option map for icons that are used for the current conditions
         const icon = {
             0: {image: "https://cdn-icons-png.flaticon.com/128/6974/6974827.png"},
             1: {image: "https://cdn-icons-png.flaticon.com/128/7774/7774454.png"},
@@ -46,23 +52,30 @@ fetch(weatherAPI)
             75: {image: "https://cdn-icons-png.flaticon.com/128/6363/6363293.png"},
         }
 
+        //Gets weather code and sends image that matches the current condition
         const weatherIcon = icon[weatherCode]
         document.querySelector('#weatherIcon').src = weatherIcon.image
 
+        //Gets weather code and sends what the current condition is
         const conditionTxt = conditions[weatherCode]
         document.querySelector('#conditions').textContent = conditionTxt
 
+        //Alters font size of data being pulled from API
         document.querySelector('#temp').style.fontSize = "30px"
         document.querySelector('#humi').style.fontSize = "30px"
         document.querySelector('#conditions').style.fontSize = "30px"
 
+        //Changes color of temp text based on if it is hot or cold
+        //Changes icon next to temp if its hot or cold
         if(objData.current.temperature_2m > 80)
         {
             document.querySelector('#temp').style.color = "red"
+            document.querySelector('#tempIcon').src = "https://cdn-icons-png.flaticon.com/128/1684/1684426.png"
         }
         else
         {
             document.querySelector('#temp').style.color = "blue"
+            document.querySelector('#tempIcon').src = "https://cdn-icons-png.flaticon.com/128/2322/2322701.png"
         }
 
     })
